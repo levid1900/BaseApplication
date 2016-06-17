b5b7fb18701d1ced85ba82077884d6f9f9bfc7c8
Levid <levid1900@gmail.com>
2016-06-17 18:42:45 +0800

    修改打印Log方法

--- app/src/main/java/com/baseapplication/utils/LLog.java
+++ app/src/main/java/com/baseapplication/utils/LLog.java
@@ -1,64 +1,86 @@
 package com.baseapplication.utils;
 
-import android.util.Log;
-
 import com.baseapplication.BuildConfig;
+import com.orhanobut.logger.AndroidLogTool;
+import com.orhanobut.logger.LogLevel;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.Settings;
 
 public class LLog {
-	private static final String TAG = "com.levid";
-	private static final String EMPTY_LOG = "";
+    private static final String TAG = "com.levid";
+    private static final String EMPTY_LOG = "";
 
-	enum Level {
-		d, w, e
-	}
+    private static LLog mInstance;
 
-    private static boolean isDebugMode(){
+    public static LLog getInstance() {
+        if (mInstance == null) {
+            mInstance = new LLog();
+        }
+        return mInstance;
+    }
+
+    private LLog() {
+        Logger.init(TAG)
+                .methodCount(0)                 // default 2
+                .hideThreadInfo()               // default shown
+                .logLevel(LogLevel.FULL)        // default LogLevel.FULL
+                .methodOffset(0)                // default 0
+                .logTool(new AndroidLogTool()); // custom log tool, optional
+    }
+
+    private boolean isDebugMode() {
         return BuildConfig.DEBUG;
     }
 
-	private static String getMessage(String s) {
-		return (s == null ? EMPTY_LOG : s);
-	}
+    private String getMessage(String s) {
+        return (s == null ? EMPTY_LOG : s);
+    }
 
-	public static void d(String s) {
-		if (isDebugMode()) {
-			Log.d(TAG, getMessage(s));
-		}
-	}
-
-	public static void e(String s) {
-		if (isDebugMode()) {
-			Log.e(TAG, getMessage(s));
-		}
-	}
-
-    public static void i(String s) {
+    public void json(String json) {
         if (isDebugMode()) {
-            Log.i(TAG, getMessage(s));
+            Logger.json(getMessage(json));
         }
     }
 
-	public static void d(String tag, String message) {
-		if (isDebugMode()) {
-			Log.d(tag, getMessage(message));
-		}
-	}
+    public void d(String s) {
+        if (isDebugMode()) {
+            Logger.d(getMessage(s));
+        }
+    }
 
-	public static void w(String tag, String message) {
-		if (isDebugMode()) {
-			Log.w(tag, getMessage(message));
-		}
-	}
+    public void e(String s) {
+        if (isDebugMode()) {
+            Logger.e(getMessage(s));
+        }
+    }
 
-	public static void i(String tag, String message) {
-		if (isDebugMode()) {
-			Log.i(tag, getMessage(message));
-		}
-	}
+    public void i(String s) {
+        if (isDebugMode()) {
+            Logger.i(getMessage(s));
+        }
+    }
 
-	public static void e(String tag, String message) {
-		if (isDebugMode()) {
-			Log.e(tag, getMessage(message));
-		}
-	}
+    public void d(String tag, String message) {
+        if (isDebugMode()) {
+            Logger.t(tag).d(getMessage(message));
+        }
+    }
+
+    public void w(String tag, String message) {
+        if (isDebugMode()) {
+            Logger.t(tag).w(getMessage(message));
+        }
+    }
+
+    public void i(String tag, String message) {
+        if (isDebugMode()) {
+            Logger.t(tag).i(getMessage(message));
+        }
+    }
+
+    public void e(String tag, String message) {
+        if (isDebugMode()) {
+            Logger.t(tag).e(getMessage(message));
+        }
+    }
 }
